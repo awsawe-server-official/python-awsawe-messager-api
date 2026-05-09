7ba4ad2657b391fca4e7f74b6bfc5704df3a29b1
awsawe-server <awsawe.server.official@gmail.com>
2026-05-09 22:27:49 +0300

    bot_recv.py

---
+++ bot_recv.py
@@ -0,0 +1,13 @@
+from awsawe_messager_api import bot
+
+config = {
+    'bot_id': '123456789',
+    'bot_token': 'ваш_токен',
+    'chat_id': '987654321',
+    'include_system': 'true',     # Включить системные сообщения
+    'debug': 'true'
+}
+
+# Получение последних сообщений
+messages = bot.recv(config)
+print(messages)
