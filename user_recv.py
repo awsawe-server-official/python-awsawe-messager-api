73973eb98415d1b982f4dd12fc815e3ada42c8be
awsawe-server <awsawe.server.official@gmail.com>
2026-05-09 22:29:46 +0300

    Create user_recv.py

---
+++ user_recv.py
@@ -0,0 +1,14 @@
+from awsawe_messager_api import user
+
+config = {
+    'username': 'ваш_логин',
+    'password': 'ваш_пароль',
+    'chat_id': '987654321',
+    'include_system': 'true',      # Включить системные сообщения
+    'decrypt': 'true',              # Расшифровывать сообщения
+    'debug': 'true'
+}
+
+# Получение и расшифровка сообщений
+messages = user.recv(config)
+print(messages)
